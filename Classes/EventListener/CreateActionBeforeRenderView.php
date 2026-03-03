e96bb8e1bf1f967e9b0108216c0b89e42b85d555
vnc-stel <sajan.telrejah@vancado.com>
2026-03-03 15:03:27 +0100

    [FEATURE] Refacotor eventlistener

--- Classes/EventListener/CreateActionBeforeRenderView.php
+++ Classes/EventListener/CreateActionBeforeRenderView.php
@@ -3,7 +3,6 @@
 namespace Undkonsorten\Powermailpdf\EventListener;
 
 use FPDM;
-use In2code\Powermail\Controller\FormController;
 use In2code\Powermail\Domain\Model\Answer;
 use In2code\Powermail\Domain\Model\Field;
 use In2code\Powermail\Domain\Model\Mail;
@@ -15,208 +14,237 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
-use TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser;
 use TYPO3\CMS\Core\TypoScript\TypoScriptService;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use In2code\Powermail\Events\FormControllerCreateActionBeforeRenderViewEvent;
 
-
-/**
- * PDF handling.
- *
- */
 final class CreateActionBeforeRenderView
 {
-    protected ?bool $encoding = null;
-    
-    protected $settings = [];
+    protected ?string $encoding = null;
+    protected array $settings = [];
 
     public function __construct(
-        protected ResourceFactory $resourceFactory, 
+        protected ResourceFactory $resourceFactory,
         private StandaloneView $standaloneView,
-    ) {
-    }
+        private ConfigurationManagerInterface $configurationManager,
+    ) {}
 
-    /**
-     *
-     * @param FormControllerCreateActionBeforeRenderViewEvent $event
-     * @throws Exception
-     *
-     */
     public function __invoke(FormControllerCreateActionBeforeRenderViewEvent $event): void
     {
-        $configurationManager = GeneralUtility::makeInstance(ConfigurationManagerInterface::class);
-        $settings = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+        $fullTypoScript = $this->configurationManager
+            ->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
 
-        $this->settings = $settings['plugin.']['tx_powermailpdf.']['settings.'];
+        $this->settings = $fullTypoScript['plugin.']['tx_powermailpdf.']['settings.'] ?? [];
+
+        if (empty($this->settings['enablePowermailPdf'])) {
+            return;
+        }
+
+        if (!empty($this->settings['sourceFile'])) {
+            if (!file_exists(GeneralUtility::getFileAbsFileName($this->settings['sourceFile']))) {
+                throw new \Exception(
+                    "The file does not exist: " . $this->settings['sourceFile'],
+                    1417520887
+                );
+            }
+        }
+
         $mail = $event->getMail();
         $formController = $event->getFormController();
 
-        if ($this->settings['enablePowermailPdf']) {
-            if ($this->settings['sourceFile']) {
-                if (!file_exists(GeneralUtility::getFileAbsFileName($this->settings['sourceFile']))) {
-                    throw new \Exception("The file does not exist: " . $this->settings['sourceFile'] . " Please set correct path in plugin.tx_powermailpdf.settings.sourceFile", 1417520887);
-                }
-            }
+        $powermailPdfFile = null;
 
-            if ($this->settings['fillPdf']) {
-                $powermailPdfFile = $this->generatePdf($mail);
-            } else {
-                $powermailPdfFile = null;
-            }
+        if (!empty($this->settings['fillPdf'])) {
+            $powermailPdfFile = $this->generatePdf($mail);
+        }
 
-            if ($this->settings['showDownloadLink']) {
-                $label = LocalizationUtility::translate("download", "powermailpdf");
-                //Adds a field for the download link at the thx site
-                /* @var $answer Answer */
-                $answer = GeneralUtility::makeInstance(Answer::class);
-                /* @var $field Field */
-                $field = GeneralUtility::makeInstance(Field::class);
-                $field->setTitle(LocalizationUtility::translate('downloadLink', 'powermailpdf'));
-                $field->setMarker('downloadLink');
-                $field->setType('downloadLink');
-                $answer->setField($field);
-                $answer->setValue($this->render($powermailPdfFile, $label));
-                $mail->addAnswer($answer);
-            }
+        if (!empty($this->settings['showDownloadLink']) && $powermailPdfFile) {
+            $label = LocalizationUtility::translate("download", "powermailpdf");
 
-            if ($this->settings['email.']['attachFile']) {
-                // set pdf filename for attachment via TypoScript
-                $settings = $formController->getSettings();
-                $settings['receiver']['addAttachment']['value'] = $powermailPdfFile->getForLocalProcessing(false);
-                $settings['sender']['addAttachment']['value'] = $powermailPdfFile->getForLocalProcessing(false);
-                $formController->setSettings($settings);
-            }
+            $answer = GeneralUtility::makeInstance(Answer::class);
+            $field = GeneralUtility::makeInstance(Field::class);
+
+            $field->setTitle(
+                LocalizationUtility::translate('downloadLink', 'powermailpdf')
+            );
+            $field->setMarker('downloadLink');
+            $field->setType('downloadLink');
+
+            $answer->setField($field);
+            $answer->setValue($this->render($powermailPdfFile, $label));
+
+            $mail->addAnswer($answer);
+        }
+
+        if (!empty($this->settings['email.']['attachFile']) && $powermailPdfFile) {
+            $settings = $formController->getSettings();
+
+            $settings['receiver']['addAttachment']['value']
+                = $powermailPdfFile->getForLocalProcessing(false);
+
+            $settings['sender']['addAttachment']['value']
+                = $powermailPdfFile->getForLocalProcessing(false);
+
+            $formController->setSettings($settings);
         }
     }
 
-    /**
-     * @param Mail $mail
-     * @return File
-     * @throws Exception
-     */
-    protected function generatePdf(Mail $mail)
+    protected function generatePdf(Mail $mail): File
     {
-        $this->encoding = $this->settings['encoding'];
+        $this->encoding = $this->settings['encoding'] ?? null;
 
         /** @var Folder $folder */
-        $folder = $this->resourceFactory->getFolderObjectFromCombinedIdentifier($this->settings['target.']['pdf']);
+        $folder = $this->resourceFactory
+            ->getFolderObjectFromCombinedIdentifier($this->settings['target.']['pdf']);
 
-        // Include \FPDM library from phar file, if not included already (e.g. composer installation)
         if (!class_exists('\FPDM')) {
-            @include 'phar://' . ExtensionManagementUtility::extPath('powermailpdf') . 'Resources/Private/PHP/fpdm.phar/vendor/autoload.php';
+            @include 'phar://' . ExtensionManagementUtility::extPath('powermailpdf')
+                . 'Resources/Private/PHP/fpdm.phar/vendor/autoload.php';
         }
 
-        //Normal Fields
-        $fieldMap = $this->settings['fieldMap.'];
-
+        $fieldMap = $this->settings['fieldMap.'] ?? [];
         $answers = $mail->getAnswers();
 
-        $fdfDataStrings = array();
-        $pdfField_value = null;
+        $fdfDataStrings = [];
+
         foreach ($fieldMap as $fieldID => $fieldConfig) {
 
+            $pdfFieldName = explode('.', $fieldID)[0];
+            $fdfDataStrings[$pdfFieldName] = 'k.A.';
+
             if (is_array($fieldConfig)) {
-                $pdfField_type = $fieldConfig['type'];
-                $pdfField_value = $fieldConfig['form_value'];
-                $formField_name = $fieldConfig['form_name'];
+                $pdfFieldType = $fieldConfig['type'] ?? 'text';
+                $formFieldName = $fieldConfig['form_name'] ?? '';
+                $formValue = $fieldConfig['form_value'] ?? null;
+                $pdfValue = $fieldConfig['pdf_value'] ?? null;
             } else {
-                $pdfField_type = 'text';
-                $formField_name = $fieldConfig;
+                $pdfFieldType = 'text';
+                $formFieldName = $fieldConfig;
+                $formValue = null;
+                $pdfValue = null;
             }
-
-            $pdfField_name = explode('.', $fieldID)[0];
-
-            $fdfDataStrings[$pdfField_name] = 'k.A.';
 
             foreach ($answers as $answer) {
 
-                if ($formField_name == $answer->getField()->getMarker()) {
-                    if ($pdfField_type == 'text') {
-                        $pdfField_value = $this->encodeValue($answer->getValue());
-                    } elseif ($pdfField_type == 'checkbox') {
-                        if ($answer->getValue() == $fieldConfig['form_value']) {
-                            $pdfField_value = $this->encodeValue($fieldConfig['pdf_value']);
-                        }
-                    }
-                } else {
+                if ($formFieldName !== $answer->getField()->getMarker()) {
                     continue;
                 }
 
-                if (!empty($pdfField_value)) {
-                    $fdfDataStrings[$pdfField_name] = $pdfField_value;
+                $answerValue = $answer->getValue();
+
+                if ($pdfFieldType === 'text') {
+                    $fdfDataStrings[$pdfFieldName] = $this->encodeValue($answerValue);
+                }
+
+                elseif ($pdfFieldType === 'checkbox') {
+
+                    if (is_array($answerValue)) {
+                        if (in_array($formValue, $answerValue, true)) {
+                            $fdfDataStrings[$pdfFieldName] =
+                                $this->encodeValue($pdfValue);
+                        }
+                    } else {
+                        if ($answerValue == $formValue) {
+                            $fdfDataStrings[$pdfFieldName] =
+                                $this->encodeValue($pdfValue);
+                        }
+                    }
                 }
             }
         }
 
-        // Variables
-        if (isset($this->settings['variables.'])) {
-            $variables = $this->settings['variables.'];
+        if (!empty($this->settings['variables.'])) {
+            $typoScriptService = GeneralUtility::makeInstance(TypoScriptService::class);
+            $variables = $typoScriptService->convertTypoScriptArrayToPlainArray(
+                $this->settings['variables.']
+            );
 
-            if (!empty($variables)) {
-                $typoScriptService = GeneralUtility::makeInstance(TypoScriptService::class);
-                $variables = $typoScriptService->convertTypoScriptArrayToPlainArray($variables);
-                $cObject = GeneralUtility::makeInstance(ContentObjectRenderer::class);
-                foreach ($variables as $key => $item) {
-                    $type = $item['_typoScriptNodeValue'];
-                    unset($item['_typoScriptNodeValue']);
-                    $fdfDataStrings[$key] = $cObject->cObjGetSingle($type, $item);
-                }
+            $cObject = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+
+            foreach ($variables as $key => $item) {
+                $type = $item['_typoScriptNodeValue'] ?? '';
+                unset($item['_typoScriptNodeValue']);
+                $fdfDataStrings[$key] = $cObject->cObjGetSingle($type, $item);
             }
         }
 
         $pdfOriginal = GeneralUtility::getFileAbsFileName($this->settings['sourceFile']);
 
-        if (!empty($pdfOriginal)) {
-            $pdfFlatTempFile = (string) null;
-            $info = pathinfo($pdfOriginal);
-            $pdfFilename = basename($pdfOriginal, '.' . $info['extension']) . '_';
-            $pdfTempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
-
-            $pdf = new \FPDM($pdfOriginal);
-            $pdf->Load($fdfDataStrings, !$this->encoding); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
-            $pdf->Merge();
-            $pdf->Output("F", GeneralUtility::getFileAbsFileName($pdfTempFile));
-
-            if ($this->settings['flatten'] && $this->settings['flattenTool']) {
-                $pdfFlatTempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
-                $tempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
-                switch ($this->settings['flattenTool']) {
-                    case 'gs':
-                        // Flatten PDF with ghostscript
-                        @shell_exec("gs -sDEVICE=pdfwrite -dSubsetFonts=false -dPDFSETTINGS=/default -dNOPAUSE -dBATCH -sOutputFile=" . $pdfFlatTempFile . " " . $pdfTempFile);
-                        break;
-                    case 'pdftocairo':
-                        // Flatten PDF with pdftocairo
-                        @shell_exec('pdftocairo -pdf ' . $pdfTempFile . ' ' . $pdfFlatTempFile);
-                        break;
-                    case 'pdftk':
-                        // Flatten PDF with pdftk
-                        @shell_exec('pdftk ' . $pdfTempFile . ' generate_fdf output ' . $tempFile);
-                        @shell_exec('pdftk ' . $pdfTempFile . ' fill_form ' . $tempFile . ' output ' . $pdfFlatTempFile . ' flatten');
-                        break;
-                }
-            }
-        } else {
-            throw new Exception("No pdf file is set in Typoscript. Please set tx_powermailpdf.settings.sourceFile if you want to use the filling feature.", 1417432239);
+        if (empty($pdfOriginal)) {
+            throw new Exception(
+                "No pdf file is set in TypoScript.",
+                1417432239
+            );
         }
 
-        if (file_exists($pdfFlatTempFile)) {
+        $pdfFlatTempFile = null;
+        $info = pathinfo($pdfOriginal);
+        $pdfFilename = basename($pdfOriginal, '.' . $info['extension']) . '_';
+        $pdfTempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
+
+        $pdf = new \FPDM($pdfOriginal);
+        $pdf->Load($fdfDataStrings, !$this->encoding);
+        $pdf->Merge();
+        $pdf->Output("F", GeneralUtility::getFileAbsFileName($pdfTempFile));
+
+        if (!empty($this->settings['flatten']) && !empty($this->settings['flattenTool'])) {
+
+            $pdfFlatTempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
+            $tempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
+
+            switch ($this->settings['flattenTool']) {
+
+                case 'gs':
+                    @shell_exec(
+                        "gs -sDEVICE=pdfwrite -dSubsetFonts=false -dPDFSETTINGS=/default "
+                        . "-dNOPAUSE -dBATCH -sOutputFile="
+                        . escapeshellarg($pdfFlatTempFile)
+                        . " "
+                        . escapeshellarg($pdfTempFile)
+                    );
+                    break;
+
+                case 'pdftocairo':
+                    @shell_exec(
+                        'pdftocairo -pdf '
+                        . escapeshellarg($pdfTempFile)
+                        . ' '
+                        . escapeshellarg($pdfFlatTempFile)
+                    );
+                    break;
+
+                case 'pdftk':
+                    @shell_exec(
+                        'pdftk '
+                        . escapeshellarg($pdfTempFile)
+                        . ' generate_fdf output '
+                        . escapeshellarg($tempFile)
+                    );
+                    @shell_exec(
+                        'pdftk '
+                        . escapeshellarg($pdfTempFile)
+                        . ' fill_form '
+                        . escapeshellarg($tempFile)
+                        . ' output '
+                        . escapeshellarg($pdfFlatTempFile)
+                        . ' flatten'
+                    );
+                    break;
+            }
+        }
+
+        if ($pdfFlatTempFile && file_exists($pdfFlatTempFile)) {
             return $folder->addFile($pdfFlatTempFile);
         }
 
         return $folder->addFile($pdfTempFile);
     }
 
-    /**
-     * @param File $file
-     * @param $label
-     * @return mixed
-     */
-    protected function render(File $file, $label)
+    protected function render(File $file, string $label)
     {
         $templatePath = GeneralUtility::getFileAbsFileName($this->settings['template']);
+
         $this->standaloneView->setFormat('html');
         $this->standaloneView->setTemplatePathAndFilename($templatePath);
         $this->standaloneView->assignMultiple([
@@ -227,15 +255,18 @@ final class CreateActionBeforeRenderView
         return $this->standaloneView->render();
     }
 
-    protected function encodeValue($value)
+    protected function encodeValue(mixed $value): string
     {
         if (is_array($value)) {
-            $value = implode(',', $value);
+            $value = implode(', ', $value);
         }
+
+        $value = (string)$value;
+
         if ($this->encoding) {
             return iconv('UTF-8', $this->encoding, $value);
-        } else {
-            return $value;
         }
+
+        return $value;
     }
 }
